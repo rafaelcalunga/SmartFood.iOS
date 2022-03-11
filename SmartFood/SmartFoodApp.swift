1c30405a5f008d740f7d30890eb3d77cb92c1f58
Rafael Calunga <rafaelcalunga@gmail.com>
2022-03-11 14:30:34 -0500

    Initial Commit

---
+++ SmartFood/SmartFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFoodApp.swift
+//  SmartFood
+//
+//  Created by Rafael Calunga on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct SmartFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
